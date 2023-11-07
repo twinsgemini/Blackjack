596d9ebf62db935d52397f9488862a6e770e0c14
twinsgemini <r_sharon@naver.com>
2023-11-07 22:00:47 +0900

    자바 객체지향 공부 - Blackjack Game - Day 2

--- src/study/Game.java
+++ src/study/Game.java
@@ -3,6 +3,7 @@ package study;
 import java.util.Scanner;
 
 public class Game {
+    private static final int INIT_RECEIVE_CARD_COUNT = 2;
 
     public void play() {
         System.out.println("===== Blackjack =====");
@@ -13,7 +14,30 @@ public class Game {
         Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
-        System.out.println(cardDeck.toString());
+        initPhase(cardDeck, gamer);
+        playingPhase(sc, cardDeck, gamer);
+    }
 
+    private void initPhase(CardDeck cardDeck, Gamer gamer) {
+        System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
+        for (int i=0; i<INIT_RECEIVE_CARD_COUNT; i++) {
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
+    }
+
+    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
+        String gamerInput;
+        while (true) {
+            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
+            gamerInput = sc.nextLine();
+
+            if ("0".equals(gamerInput)) {
+                break;
+            }
+
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
     }
 }
--- src/study/Gamer.java
+++ src/study/Gamer.java
@@ -1,13 +1,40 @@
 package study;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Gamer {
     private List<Card> cards;
 
-    public void receiveCard(Card card) {}
+    public Gamer() {
+        cards = new ArrayList<>();
+    }
+
+    /**
+     * 카드 받기
+     * @param card
+     */
+    public void receiveCard(Card card) {
+        this.cards.add(card);
+        this.showCards();
+    }
+
+    /**
+     * 현재 보유한 카드 확인
+     */
+    public void showCards() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유 카드 목록 \n");
+
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
 
     public List<Card> openCards() {
-        return null;
+        return this.cards;
     }
 }
